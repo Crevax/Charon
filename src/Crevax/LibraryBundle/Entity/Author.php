abdefca4b5a3f0c0488ece8f614b09584ac4b8f4
CJ Davis <cj@cjdavis.me>
2014-06-05 18:16:37 +0000

    Change Book and Author list to be tables

--- src/Crevax/LibraryBundle/Entity/Author.php
+++ src/Crevax/LibraryBundle/Entity/Author.php
@@ -3,6 +3,7 @@
 
 namespace Crevax\LibraryBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
