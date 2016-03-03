977d66a6130e7bf4229a03594407f3ff803980ff
CJ Davis <cj@cjdavis.me>
2016-03-02 22:30:01 -0500

    Create 'Hello World' with React, using webpack to handle fancy js setup

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: './src/js/app.jsx',
+    vendors: ['react', 'react-dom']
+  },
+  output: {
+    path: './dist/js/',
+    publicPath: '/js/',
+    filename: 'app.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        exclude: '/node_modules/',
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      minimize: true
+    })
+  ]
+};
