6e8628ea997d669fca78f9a4b77d460a8c90eac0
CJ Davis <cj@cjdavis.me>
2016-03-07 21:24:22 -0500

    Add redux store

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
-render(
-  <h1>Hello World!</h1>,
-  document.getElementById('app')
-)
+render((
+  <Provider store={store} >
+    <h1>Hello World!</h1>
+  </Provider>
+), document.getElementById('app'))
