f02f004a4bceb4223e0bf39e7e8311219d4a971e
CJ Davis <cj@cjdavis.me>
2016-11-08 21:50:53 -0500

    Add logging of redux store changes when running the dev server

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
 import { routeState } from '../components/routing';
 import { repos } from '../components/github';
 import { authors } from '../components/library';
@@ -10,6 +11,19 @@ const rootReducer = combineReducers({
   authors
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const configureStore = () => {
+  const middlewares = [thunk];
+
+  if (process.env.NODE_ENV && process.env.NODE_ENV !== 'production') {
+    middlewares.push(createLogger());
+  }
+
+  return createStore(
+    rootReducer,
+    applyMiddleware(...middlewares)
+  );
+}
+
+const store = configureStore();
 
 export default store;
