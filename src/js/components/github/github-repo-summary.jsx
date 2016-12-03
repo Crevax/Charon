f893d2e38e86ff1c3348add6d8ad4ccd3047e409
CJ Davis <cj@cjdavis.me>
2016-12-03 13:21:52 -0500

    Add more details to Github Repo Summary (using moment.js for time parsing)

--- src/js/components/github/github-repo-summary.jsx
+++ src/js/components/github/github-repo-summary.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import moment from 'moment';
 
 const GithubRepoSummary = ({repo}) => (
-  <h3>{ repo.name }</h3>
+  <div>
+    <h3>{ repo.name }</h3>
+    <div>
+      <div>Language: { repo.language }</div>
+      <div>Last Pushed: { moment(repo.pushed_at).fromNow() }</div>
+    </div>
+  </div>
 );
 
 export default GithubRepoSummary;
